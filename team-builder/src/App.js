d328812cc4fd18c2960c581b71298ac006649b7a
Anna W <anski.anna@gmail.com>
2019-09-18 18:05:02 +0100

    app.js

---
+++ team-builder/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Team from './components/Team';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Team /> 
+    </div>
+  );
+}
+
+export default App;
